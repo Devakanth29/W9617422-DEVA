27fc1a7368dab2982583fc26f7dcfbdf217bc6f4
Devakanth29 <148253356+Devakanth29@users.noreply.github.com>
2024-06-03 18:43:00 +0530

    Add files via upload

---
+++ Watchwave/app/src/main/java/uk/ac/tees/mad/w9617422/MainActivity.kt
@@ -0,0 +1,55 @@
+ package uk.ac.tees.mad.w9617422
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import uk.ac.tees.mad.w9617422.navUtils.Navigation
+import uk.ac.tees.mad.w9617422.ui.theme.WatchwaveTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WatchwaveTheme(darkTheme = false) {
+                val navController = rememberNavController()
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier =Modifier.padding(innerPadding)) {
+                        Navigation(navController)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WatchwaveTheme {
+        Greeting("Android")
+    }
+}
